695418bcd5257345499f043cb4f436328d080273
muza <muzallyefah@gmail.com>
2023-11-12 18:37:34 +0800

    fix: convert pdf to txt

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const multer = require("multer");
 const ResumeParser = require("./src");
 const path = require('path');
+const pdf = require("pdf-parse-fork");
+const fs = require('fs');
 
 const app = express();
 const cors = require("cors");
@@ -23,20 +25,25 @@ app.use(cors());
 
 app.post("/api/extract-resume", upload.single('file'), async (req, res) => {
   const file = req.file;
-
+ 
   if (!file) {
-    return res.status(400).json({ message: 'No file uploaded' });
+   return res.status(400).json({ message: 'No file uploaded' });
   }
-
+ 
+  const dataBuffer = fs.readFileSync(file.path);
+  const data = await pdf(dataBuffer);
+  const txtFileName = path.join('uploads', `${path.basename(file.path, path.extname(file.path))}.txt`);
+  fs.writeFileSync(txtFileName, data.text);
+ 
   try {
-    const resume = new ResumeParser(file.path);
-    const data = await resume.parseToJSON();
-    res.json(data);
+   const resume = new ResumeParser(txtFileName);
+   const resumeData = await resume.parseToJSON();
+   res.json(resumeData);
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Error parsing resume' });
+   console.error(error);
+   res.status(500).json({ message: 'Error parsing resume' });
   }
-});
+ }); 
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
